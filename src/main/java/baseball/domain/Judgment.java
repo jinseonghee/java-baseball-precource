1297da2836c8836db2880e0c96975f37ffb92d6a
jinseonghee <wlstjdgml0610@gmail.com>
2021-09-30 16:18:29 +0900

    create skeleton code

---
+++ src/main/java/baseball/domain/Judgment.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgment {
+
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        return 0;
+    }
+
+    public boolean hasPlace(int placeNumber, int number) { //특정 자리, 특정 숫자가 있는지
+        return false;
+    }
+}
---
+++ src/main/java/baseball/domain/NubmerGenerator.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class NubmerGenerator {
+
+    public List<Integer> createRandomNumbers() { //collection을 사용하여 배열을 지정해줄 필요가 없다.
+        return null;
+    }
+}
