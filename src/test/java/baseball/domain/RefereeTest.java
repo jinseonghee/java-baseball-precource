bf21f8fde290822591172d9a630d599455b7859c
jinseonghee <wlstjdgml0610@gmail.com>
2021-10-06 18:48:31 +0900

    modified number comparison test code

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,32 +1,39 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 class RefereeTest {
 
+    private static final List<Integer> ANSWER = Arrays.asList(1, 2, 3);
+    private Referee referee;
+
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
+    }
+
     @Test
     void 스트라이크3() {
-        Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
     }
 
     @Test
     void 아웃() {
-        Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
+        String result = referee.compare(ANSWER, Arrays.asList(7, 8, 9));
         assertThat(result).isEqualTo("아웃");
     }
 
     @Test
     void 볼3() {
-        Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
+        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
         assertThat(result).isEqualTo("3 볼 0 스트라이크");
     }
 }
