15a4e3f5cd7418ec85de7eb90defd58b3d1eb2de
jinseonghee <wlstjdgml0610@gmail.com>
2021-10-06 19:40:14 +0900

    refactor : duplicate code modification

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,7 +1,8 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,14 @@ class RefereeTest {
         referee = new Referee();
     }
 
+    @ParameterizedTest //여러가지 경우에 대해 test 가능
+    @CsvSource({"1,2,3,0 볼 3 스트라이크", "7,8,9,아웃", "2,3,1,3 볼 0 스트라이크", "1,3,2,2 볼 1 스트라이크"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(actual).isEqualTo(expected);
+    }
+}
+    /*
     @Test
     void 스트라이크3() {
         String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
@@ -36,4 +45,11 @@ class RefereeTest {
         String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
         assertThat(result).isEqualTo("3 볼 0 스트라이크");
     }
-}
+
+
+    @Test
+    void 볼2_스트라이1() {
+        String result = referee.compare(ANSWER, Arrays.asList(1, 3, 2));
+        assertThat(result).isEqualTo("2 볼 1 스트라이크");
+    }
+     */
